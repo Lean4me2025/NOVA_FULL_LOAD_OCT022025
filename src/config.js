7ce0847a556c9c7abee561d317ef91df66eb786d
Drews Project uploads <drewhrs2@gmail.com>
2025-10-10 10:11:20 -0700

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,15 @@
+window.NOVA_CONFIG = {
+  VERSION: "2.4",
+  DATA: {
+    SECTORS: "../data/sector_traits.json",
+    JOBS: "../data/jobs.json"
+  },
+  NAVI_ENDPOINT: "",
+  PAYHIP: {
+    STARTER: "https://payhip.com/b/StarterPlan",
+    PRO: "https://payhip.com/b/ProPlan",
+    MASTERY: "https://payhip.com/b/MasteryPlan",
+    BOOK: "https://payhip.com/b/HowToKnowYourPurpose",
+    UTM: "?utm_source=nova&utm_medium=web&utm_campaign=invest_page"
+  }
+};
